18a42a66b071dc6e2d0b1627fa20624a0ca338d5
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-08-28 19:19:26 +0530

    modified math.py in .

--- math.py
+++ math.py
@@ -1,4 +1,11 @@
+import math
+
 print(5/2)
 print(5//2)
 print(-3//2)
 print(int(-3/2))
+
+print(math.floor(3/2)) #round down
+print(math.ceil(3/2)) #round up
+print(math.sqrt(2))
+print(math.pow(2,3))
